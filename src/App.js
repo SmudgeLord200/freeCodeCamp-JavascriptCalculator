f3d64de5f0c6597b008ff0f4d60d94d879e86923
Nicole <nicoleho1314@gmail.com>
2023-11-01 12:45:03 +0800

    first draft

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+
+const calcData = [
+  { id: "clear", value: "AC" },
+  { id: "divide", value: "/", className: 'operator' },
+  { id: "multiply", value: "x", className: 'operator' },
+  { id: "seven", value: 7 },
+  { id: "eight", value: 8 },
+  { id: "nine", value: 9 },
+  { id: "subtract", value: "-", className: 'operator' },
+  { id: "four", value: 4 },
+  { id: "five", value: 5 },
+  { id: "six", value: 6 },
+  { id: "add", value: "+", className: 'operator' },
+  { id: "one", value: 1 },
+  { id: "two", value: 2 },
+  { id: "three", value: 3 },
+  { id: "equals", value: "=" },
+  { id: "zero", value: 0 },
+  { id: "decimal", value: "." },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div id="calculator">
+        <div id="output">
+          <div id="formula">Forumla</div>
+          <div id="display">Display</div>
+        </div>
+        <div id="calculator-pad">
+          {calcData.map((cal) => {
+            return (
+              <button id={cal.id} className={cal.className}>{cal.value}</button>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
